ef2a5b3775ca1fe37406c242126222aa6ea2ab1e
KanadKJ <jadhav.kanadkj@gmail.com>
2025-03-16 18:33:51 +0530

    Home

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -3,7 +3,9 @@ import bgvector from "../Assets/BackgroundPattern.png";
 import bgvectorBlank from "../Assets/bg_vector.png";
 import blueBg from "../Assets/onlyBlueBg.png";
 import Ribbons from "./Ribbons";
-export default function Background({ type, lifed, show }) {
+import illLeft from "../Assets/illLeft.png";
+import char from "../Assets/Characters.png";
+export default function Background({ type, lifed, show, isHome }) {
   console.log(show);
 
   const bg = {
@@ -13,6 +15,21 @@ export default function Background({ type, lifed, show }) {
   };
   return (
     <div className="absolute top-0 left-0 h-full w-full -z-10 ">
+      {isHome === "yes" && (
+        <div className="w-full flex justify-between absolute top-0">
+          <div>
+            <img
+              src={illLeft}
+              alt="illLeft"
+              className="h-96 relative -left-40"
+            />
+          </div>
+
+          <div>
+            <img src={char} alt="char" className="h-72" />
+          </div>
+        </div>
+      )}
       <img
         className={
           lifed === "up"
--- src/Screens/HomePage.jsx
+++ src/Screens/HomePage.jsx
@@ -6,79 +6,24 @@ export default function HomePage() {
   const { userData } = useSelector((s) => s.auth);
 
   return (
-    <div className="mt-16 ">
-      <Background type={"default"} />
-      <div>
-        <h1> fadHome</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
-        <h1>Home</h1>
+    <div className="mt-36 px-6 md:px-12 lg:px-24 xl:px-32 mb-10 z-40 h-screen">
+      <Background type={"default"} show={"yes"} isHome="yes" />
+      <div className="flex">
+        <div className="flex flex-col justify-center items-center gap-4">
+          <div className="w-3/4">
+            <h1 className="font-extrabold text-5xl flex-1 text-center text-wrap">
+              <span className="text-[#0554F2]">Tender</span> Sourcing Platform
+              for Contractors in<span> </span>
+              <span className="text-[#0554F2]">West Bengal</span>
+            </h1>
+          </div>
+          <div className="text-2xl text-[#565656] flex-1 text-center text-wrap">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna ali
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
