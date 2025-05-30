7f8f7001090bd8da17d6902cc97036e5005338ce
KanadKJ <jadhav.kanadkj@gmail.com>
2025-05-31 01:33:17 +0530

    Fingerprint added

--- src/Api/TMAPI.js
+++ src/Api/TMAPI.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { TMAPI_BASE_URL } from "../Utils/CommonUtils";
+import FingerprintJS from "@fingerprintjs/fingerprintjs";
 
 export const TMGetApi = axios.create({
   baseURL: TMAPI_BASE_URL,
@@ -8,11 +9,21 @@ export const TMGetApi = axios.create({
   },
 });
 
+const getFingerprint = async () => {
+  const fp = await FingerprintJS.load();
+  const result = await fp.get();
+  return result.visitorId;
+};
+
 TMGetApi.interceptors.request.use(
-  (config) => {
+  async (config) => {
+    const fp = await getFingerprint();
     const token = JSON.parse(localStorage.getItem("user"))?.acceessToken;
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    if (fp) {
+      config.headers["Uid"] = `${fp}`;
     }
     return config;
   },
